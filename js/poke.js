a4b65ffbc04e418f742d01d331592a8afe10aa32
Wilson Parrish <wilsonparrishmk2@gmail.com>
2016-04-12 17:53:29 -0500

    working without http

--- js/poke.js
+++ js/poke.js
@@ -5,19 +5,23 @@
             .Component({
                 selector: 'poke-comp'
                 // , providers: [ng.HTTP_PROVIDERS]
+                , providers: [app.pokeService]
             })
             .View({
-                template: '<h4 >{{poke.name))</h4>'
+                template: '<div *ngFor="#poke of pokemon"><h4>{{poke.name}}</h4><img src={{poke.gif}}/></div>'
                 , directives: [ng.common.NgFor]
             })
             .Class({
-                constructor: [ng.Http, function(http) {
-                    this.getPokemon = function(){
-                        return http.get('http://pokeapi.co/api/v2/pokemon/110/')
-                    }
-                    this.pokemon = this.getPokemon();
-                    
+                constructor: [app.pokeService, function(svc){
+                    console.log(11111111, svc);
+                    this.pokemon = svc.whosThatPokemon;
                 }]
+                // constructor: [ng.Http, function(http) {
+                //     this.getPokemon = function(){
+                //         return http.get('http://pokeapi.co/api/v2/pokemon/110/')
+                //     }
+                //     this.pokemon = this.getPokemon();
+                // }]
             }) 
     
 })(window.app || (window.app = {}));
--- js/pokeService.js
+++ js/pokeService.js
@@ -2,9 +2,24 @@
     app.pokeService =
         ng.core
             .Class({
-                constructor: function() { },
-                getPokemon: function() {
-                    return ng.Http.get('http://pokeapi.co/api/v2/pokemon/110/');
-                }
+                constructor: function() { 
+                    this.whosThatPokemon = [
+                        {
+                            name: 'Bulbasaur',
+                            id: 1,
+                            gif: 'http://sprites.pokecheck.org/i/001.gif'
+                        },
+                        {
+                            name: 'Charmandar',
+                            id: 4,
+                            gif: 'http://sprites.pokecheck.org/i/004.gif'    
+                        },
+                        {
+                            name: 'Squirtle',
+                            id: 7,
+                            gif: 'http://sprites.pokecheck.org/i/007.gif'    
+                        }
+                    ]
+                },
             });
 })(window.app || (window.app = {}));
